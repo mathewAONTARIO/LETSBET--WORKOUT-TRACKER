64c4ebea2a14688097aec0fd473ed13b1154b302
Mathew Arogbo <mathew.arogbo@ontariotechu.net>
2025-11-22 21:27:26 -0500

    final cleanup

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Workout = require('../models/Workout');
 
-// HOME PAGE
 router.get('/', async (req, res) => {
   try {
     const today = new Date();
