2ee0f3100139eb4734cd54d16e95fcf600d03c69
Mathew Arogbo <mathew.arogbo@ontariotechu.net>
2025-11-24 17:43:12 -0500

    Fix Mongo URI and DB connect logic

--- config/db.js
+++ config/db.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log('mongo connected'))
-    .catch((err) => {
-      console.log('db error:', err.message);
-      process.exit(1);
+async function connectDB() {
+  try {
+    let uri = process.env.MONGO_URI;
+
+    if (!uri) {
+      throw new Error('MONGO_URI is not defined in environment variables');
+    }
+
+    // If someone accidentally pasted "MONGO_URI=..." into the value,
+    // strip everything before the first "mongodb".
+    const idx = uri.indexOf('mongodb');
+    if (idx > 0) {
+      uri = uri.slice(idx);
+    }
+
+    console.log('Connecting to MongoDB with URI:', uri.slice(0, 40) + '...');
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-};
+
+    console.log('mongo connected');
+  } catch (err) {
+    console.error('db error:', err.message);
+    process.exit(1);
+  }
+}
 
 module.exports = connectDB;
